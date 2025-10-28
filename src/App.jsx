980ceaf737650b471dcc5c4026695d596c9ba4fa
Kabelo Ramolotja <kr14rams@gmail.com>
2025-10-28 12:15:51 +0200

    react app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello from Mye-react-app</h1>
+      <p>This app uses React + Vite (JavaScript)</p>
+    </div>
+  );
+}
